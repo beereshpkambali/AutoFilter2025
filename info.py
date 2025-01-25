f502c295a106a8da054745dff69e763d5d107684
A Jakhar <125469531+jakharamit44@users.noreply.github.com>
2025-01-25 14:25:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '17737898'))
-API_HASH = environ.get('API_HASH', 'ad762fe0516f367115ba651d929cf429')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5133761076:AAFr63lQa3JG3zu-WDws-mAKW1ybr3qLITU")
+API_ID = int(environ.get('API_ID', '1234567'))
+API_HASH = environ.get('API_HASH', 'hdiet3nvhjyu34tbivbbt3bty')
+BOT_TOKEN = environ.get('BOT_TOKEN', "1234567:bhryt4bf3v")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 99999))
@@ -28,7 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5076949930').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001758277681').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-100').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5076949930').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -43,16 +43,16 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://amit:amit@cluster0.wvsjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "amit")
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "autofilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'terashort.cloud')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e3bed6f26b2557f38deba2570b25b638f51b3f2f')
+SHORTLINK_API = environ.get('SHORTLINK_API', '8752hr6325f6g3934h')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'terashort.cloud')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'e3bed6f26b2557f38deba2570b25b638f51b3f2f')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '87536893bgb346689g')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
@@ -63,7 +63,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/anumitultrabots')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/anumitultrabots')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Anumitultrabots')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001631059705'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
