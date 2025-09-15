6d92913a811c5b04117850fa7fa1c8c988432304
Mr_Abnormal <115939906+Mr-Abnormal@users.noreply.github.com>
2025-09-15 15:11:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jakharamit44/AutoFilter.git /AutoFilter
+  git clone https://github.com/Mr-Abnormal/AutoFilter2025.git /AutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilter
 fi
 cd /AutoFilter
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot ಬಾಟ್ ಚಾಲೂ...."
 python3 bot.py
